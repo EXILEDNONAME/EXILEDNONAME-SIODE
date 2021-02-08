4129708d12ab59a19452d90b2d0541a5bb2e6d05
Naufal Haidir Ridha <naufalhaidirridha@rocketmail.com>
2021-02-08 21:21:55 +0700

    Edit Page Dashboard

--- resources/views/pages/backend/main/dashboard/index.blade.php
+++ resources/views/pages/backend/main/dashboard/index.blade.php
@@ -7,7 +7,7 @@
     <div class="card card-custom wave wave-success card-stretch gutter-b">
       <div class="card-body">
         <a href="/dashboard/management/users" class="card-title font-weight-bolder font-size-h6 text-hover-state-dark"> Management Users </a><br>
-        <span class="font-weight-bolder text-muted"> 12 Users </span>
+        <span class="font-weight-bolder text-muted"> {{ \DB::table('users')->count() }} Users </span>
       </div>
     </div>
   </div>
@@ -15,7 +15,7 @@
     <div class="card card-custom wave wave-warning card-stretch gutter-b">
       <div class="card-body">
         <a href="/dashboard/management/accesses" class="card-title font-weight-bolder font-size-h6 text-hover-state-dark"> Management Accesses </a><br>
-        <span class="font-weight-bolder text-muted"> 5 Accesses </span>
+        <span class="font-weight-bolder text-muted"> {{ \DB::table('accesses')->count() }} Accesses </span>
       </div>
     </div>
   </div>
