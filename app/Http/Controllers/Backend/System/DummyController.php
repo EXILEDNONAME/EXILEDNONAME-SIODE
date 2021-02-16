f981e56cbeb74c39e02f06b349b68acd9402b313
Naufal Haidir Ridha <naufalhaidirridha@rocketmail.com>
2021-02-16 11:22:00 +0700

    Update Pages

--- app/Http/Controllers/Backend/System/DummyController.php
+++ app/Http/Controllers/Backend/System/DummyController.php
@@ -34,12 +34,32 @@ class DummyController extends Controller {
   **/
 
   public function index(Request $request) {
+
     $activity = Activity::where('subject_type', $this->model)->orderBy('created_at', 'desc')->take(5)->get();
+
+    $data_chart = $this->model::select(\DB::raw("COUNT(*) as count"))->where('created_at', 'like', \Carbon\Carbon::now()->format('Y') . '-01%')->count(); $data_chart .= ', ';
+    $data_chart .= $this->model::select(\DB::raw("COUNT(*) as count"))->where('created_at', 'like', \Carbon\Carbon::now()->format('Y') . '-02%')->count(); $data_chart .= ', ';
+    $data_chart .= $this->model::select(\DB::raw("COUNT(*) as count"))->where('created_at', 'like', \Carbon\Carbon::now()->format('Y') . '-03%')->count(); $data_chart .= ', ';
+    $data_chart .= $this->model::select(\DB::raw("COUNT(*) as count"))->where('created_at', 'like', \Carbon\Carbon::now()->format('Y') . '-04%')->count(); $data_chart .= ', ';
+    $data_chart .= $this->model::select(\DB::raw("COUNT(*) as count"))->where('created_at', 'like', \Carbon\Carbon::now()->format('Y') . '-05%')->count(); $data_chart .= ', ';
+    $data_chart .= $this->model::select(\DB::raw("COUNT(*) as count"))->where('created_at', 'like', \Carbon\Carbon::now()->format('Y') . '-06%')->count(); $data_chart .= ', ';
+    $data_chart .= $this->model::select(\DB::raw("COUNT(*) as count"))->where('created_at', 'like', \Carbon\Carbon::now()->format('Y') . '-07%')->count(); $data_chart .= ', ';
+    $data_chart .= $this->model::select(\DB::raw("COUNT(*) as count"))->where('created_at', 'like', \Carbon\Carbon::now()->format('Y') . '-08%')->count(); $data_chart .= ', ';
+    $data_chart .= $this->model::select(\DB::raw("COUNT(*) as count"))->where('created_at', 'like', \Carbon\Carbon::now()->format('Y') . '-09%')->count(); $data_chart .= ', ';
+    $data_chart .= $this->model::select(\DB::raw("COUNT(*) as count"))->where('created_at', 'like', \Carbon\Carbon::now()->format('Y') . '-10%')->count(); $data_chart .= ', ';
+    $data_chart .= $this->model::select(\DB::raw("COUNT(*) as count"))->where('created_at', 'like', \Carbon\Carbon::now()->format('Y') . '-11%')->count(); $data_chart .= ', ';
+    $data_chart .= $this->model::select(\DB::raw("COUNT(*) as count"))->where('created_at', 'like', \Carbon\Carbon::now()->format('Y') . '-12%')->count();
+
+    if (request('date_start') && request('date_end')) { $data = $this->model::orderby('created_at', 'desc')->whereBetween('created_at', [request('created_at'), request('date_end')])->select('*'); }
+    else { $data = $this->model::orderby('date_start', 'desc')->select('*'); }
+
     $data = $this->model::select('*');
     if(request()->ajax()) {
       return DataTables::eloquent($data)
       ->addColumn('action', 'includes.datatable.action')
       ->addColumn('checkbox', 'includes.datatable.checkbox')
+      ->editColumn('date_start', function($order) { return \Carbon\Carbon::parse($order->date_start)->format('d F Y, H:i'); })
+      ->editColumn('date_end', function($order) { return \Carbon\Carbon::parse($order->date_end)->format('d F Y, H:i'); })
       ->rawColumns(['action', 'checkbox'])
       ->addIndexColumn()
       ->make(true);
