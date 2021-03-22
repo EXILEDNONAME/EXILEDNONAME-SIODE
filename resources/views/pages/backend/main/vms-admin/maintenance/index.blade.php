7f47598b3a274cd26524854b94aa3c839bae8831
Naufal Haidir Ridha <naufalhaidirridha@rocketmail.com>
2021-03-22 15:09:22 +0800

    Update Layout

--- resources/views/pages/backend/main/vms-admin/maintenance/index.blade.php
+++ resources/views/pages/backend/main/vms-admin/maintenance/index.blade.php
@@ -16,14 +16,26 @@
     </div>
   </div>
 </div>
+<div class="col-md-2 my-2 my-md-0">
+  <div class="d-flex align-items-center">
+    <label class="mr-3 mb-0 d-none d-md-block">Area:</label>
+    {!! Form::select(NULL, filter_vms_areas(), NULL, ['data-column' => 4, 'placeholder' => '- Select Area -', 'class' => 'form-control filter-area']) !!}
+  </div>
+</div>
 @endpush
 
 @push('filter-function')
 d.date_start = $('#date_start').val();
 d.date_end = $('#date_end').val();
+d.filter_area = $('.filter_area').val();
 @endpush
 
 @push('filter-data')
+$('.filter-area').change(function () {
+  table.column(4)
+  .search( $(this).val() )
+  .draw();
+});
 $('#date_start').change(function () { table.draw(); });
 $('#date_end').change(function () { table.draw(); });
 @endpush
@@ -42,7 +54,7 @@ $('#date_end').change(function () { table.draw(); });
 
 @push('content-body')
 {
-  data: 'vms_type', orderable: true, 'className': 'align-middle', 'width': '1',
+  data: 'vms_type', searchable: false, orderable: false, 'className': 'align-middle', 'width': '1',
   render: function ( data, type, row ) {
     if ( data == 'Akses') { return '<span class="label label-info label-pill label-inline"> Akses </span>'; }
     else if ( data == 'Gardu' ) { return '<span class="label label-info label-pill label-inline"> Gardu </span>'; }
@@ -50,7 +62,7 @@ $('#date_end').change(function () { table.draw(); });
     else return '';
   },
 },
-{ data: 'vms_area', orderable: true, 'className': 'align-middle', 'width': '1' },
+{ data: 'vms_area', searchable: false, orderable: false, 'className': 'align-middle', 'width': '1' },
 { data: 'date_start', searchable: false, width: 1, className: 'text-nowrap' },
 { data: 'date_end', searchable: false, width: 1, className: 'text-nowrap' },
 { data: 'selisih_waktu', searchable: false, width: 1, className: 'text-nowrap' },
